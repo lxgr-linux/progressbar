2c604b1e3c891ab75219652da194963c02e12504
lxgr-linux <lxgr@protonmail.com>
2021-05-13 00:36:16 +0200

    spaces in places and assertions

--- download_demo.py
+++ download_demo.py
@@ -9,7 +9,7 @@ def bar(current, total, length):
     global time_old
     global n
     n=n+1
-    if n == 200:
+    if n == 150:
         current_old, time_old = progressbar.dload(current, total, "test", current_old, time_old)
         n=0
 
--- progressbar.py
+++ progressbar.py
@@ -14,19 +14,19 @@ def format_bytes(size):
     return str(round(size, 1))+' '+power_labels[n]
 
 def progress(i, max, string):
+    assert i <= max, "current step:"+str(i)+" is bigger than max:"+str(max)
     width, height = os.get_terminal_size()
-    pl=round(math.log(width/57 ,1.01644658))
-    string=' '+string+200*' '
+    pl = round(math.log(width/57 ,1.01644658))
+    string = ' '+string+200*' '
     print('\r('+(len(str(max))-len(str(i)))*' '+str(i)+'/'+str(max)+')'+string[:width-16+(6-2*len(str(max)))-pl]+'['+round((i/max)*pl)*'#'+(pl-round((i/max)*pl))*'-'+'] '+(3-len(str(round((i/max)*100))))*' '+str(round((i/max)*100))+'%', end='', flush=True)
 
 def dload(current, max, string, current_old, time_old):
-    time_now=time.time()
-    # print(format_bytes(current-current_old))
-    # print(time_now-time_old)
-    speed=(current-current_old)/(time_now-time_old)
+    assert current <= max, "current step:"+str(current)+" is bigger than max:"+str(max)
+    time_now = time.time()
+    speed = (current-current_old)/(time_now-time_old)
     width, height = os.get_terminal_size()
-    pl=round(math.log(width/57 ,1.01644658))
-    string=' '+string+200*' '
+    pl = round(math.log(width/57 ,1.01644658))
+    string = ' '+string+200*' '
     m, s = divmod(round((max-current)/speed), 60)
     print('\r'+string[:width-28-len(format_bytes(current))-pl]+format_bytes(current)+'iB'+(8-len(format_bytes(speed)))*' '+format_bytes(speed)+'iB/s '+(2-len(str(m)))*'0'+str(m)+':'+(2-len(str(s)))*'0'+str(s)+' ['+round((current/max)*pl)*'#'+(pl-round((current/max)*pl))*'-'+'] '+(3-len(str(round((current/max)*100))))*' '+str(round((current/max)*100))+'%', end='', flush=True)
     return current, time_now
